b7916c766bfc44d7bc5d2d819e822552c8ba22d7
satyawan <satywanbhalekar@gmail.com>
2025-06-21 20:01:12 +0530

    Basic password hashing , and token generation signup

--- src/routes/onboardingRoutes.ts
+++ src/routes/onboardingRoutes.ts
@@ -6,7 +6,7 @@ import { authenticateToken } from '../middlewares/authenticateToken';
 const router = Router();
 
 router.post('/login', OnboardingController.login); // login or signup
-router.post('/signup',authenticateToken, OnboardingController.signup); 
-router.post('/', OnboardingController.handleMessage); // conversational steps
+router.post('/signup', OnboardingController.signup); 
+router.post('/',authenticateToken, OnboardingController.handleMessage); // conversational steps
 
 export default router;
